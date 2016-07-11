dc20000a0cab17a54ceef38f615a7004868c9300
Johan Larsson <johan_gavle1@hotmail.com>
2016-07-11 20:41:05 +0200

    added a few random tests.

--- Gu.Wpf.NumericInput.UITests/DoubleBox/FormatTests.cs
+++ Gu.Wpf.NumericInput.UITests/DoubleBox/FormatTests.cs
@@ -10,55 +10,12 @@ namespace Gu.Wpf.NumericInput.UITests.DoubleBox
 
         public static readonly FormatData[] Source =
             {
-                new FormatData("1", "F1",EnUs, "1.0","1"),
-                new FormatData("1", "F1",SvSe, "1.0","1"),
-                //new FormatData(" 1", "1"),
-                //new FormatData("1 ", "1"),
-                //new FormatData(" 1 ", "1"),
-                //new FormatData("1.2", "1.2"),
-                //new FormatData("-1.2", "-1.2"),
-                //new FormatData("+1.2", "1.2"),
-                //new FormatData(".1", "0.1"),
-                //new FormatData("-.1", "-0.1"),
-                //new FormatData("0.1", "0.1"),
-                //new FormatData("1e1", "10"),
-                //new FormatData("1e0", "1"),
-                //new FormatData("1e-1", "0.1"),
-                //new FormatData("1E1", "10"),
-                //new FormatData("1E0", "1"),
-                //new FormatData("1E-1", "0.1"),
-                //new FormatData("-1e1", "-10"),
-                //new FormatData("-1e0", "-1"),
-                //new FormatData("-1e-1", "-0.1"),
-                //new FormatData("-1E1", "-10"),
-                //new FormatData("-1E0", "-1"),
-                //new FormatData("-1E-1", "-0.1"),
-            };
-
-        public static readonly FormatData[] SwedishSource =
-            {
-                //new FormatData("1", "1"),
-                //new FormatData(" 1", "1"),
-                //new FormatData("1 ", "1"),
-                //new FormatData(" 1 ", "1"),
-                //new FormatData("1,2", "1.2"),
-                //new FormatData("-1,2", "-1.2"),
-                //new FormatData("+1,2", "1.2"),
-                //new FormatData(",1", "0.1"),
-                //new FormatData("-,1", "-0.1"),
-                //new FormatData("0,1", "0.1"),
-                //new FormatData("1e1", "10"),
-                //new FormatData("1e0", "1"),
-                //new FormatData("1e-1", "0.1"),
-                //new FormatData("1E1", "10"),
-                //new FormatData("1E0", "1"),
-                //new FormatData("1E-1", "0.1"),
-                //new FormatData("-1e1", "-10"),
-                //new FormatData("-1e0", "-1"),
-                //new FormatData("-1e-1", "-0.1"),
-                //new FormatData("-1E1", "-10"),
-                //new FormatData("-1E0", "-1"),
-                //new FormatData("-1E-1", "-0.1"),
+                new FormatData("1", "F1", EnUs, "1.0","1"),
+                new FormatData("1", "F1", SvSe, "1,0","1"),
+                new FormatData("1.23456", "F3", EnUs, "1.235","1.23456"),
+                new FormatData("1.23456", "F4", EnUs, "1.2346","1.23456"),
+                new FormatData("1", "0.#", EnUs, "1","1"),
+                new FormatData("1.23456", "0.###", EnUs, "1.235","1.23456"),
             };
 
         [SetUp]
@@ -87,22 +44,50 @@ namespace Gu.Wpf.NumericInput.UITests.DoubleBox
         {
             var boxes = this.LostFocusValidateOnLostFocusBoxes;
             var doubleBox = boxes.DoubleBox;
-            this.StringFormatBox.Text = formatData.Text;
+            this.StringFormatBox.Text = formatData.StringFormat;
             this.CultureBox.Select(formatData.Culture.Name);
 
             doubleBox.Text = formatData.Text;
+            this.LoseFocusButton.Click();
 
             Assert.AreEqual(false, doubleBox.HasValidationError());
             Assert.AreEqual(formatData.Text, doubleBox.Text);
             Assert.AreEqual(formatData.Formatted, doubleBox.FormattedView().Text);
-            Assert.AreEqual("0", this.ViewModelValueBox.Text);
+            Assert.AreEqual(formatData.ViewModelValue, this.ViewModelValueBox.Text);
+            Assert.AreEqual(TextSource.UserInput, doubleBox.TextSource());
+        }
+
+
+        [Test]
+        public void WhenStringFormatChanges()
+        {
+            var boxes = this.LostFocusValidateOnLostFocusBoxes;
+            var doubleBox = boxes.DoubleBox;
+            this.StringFormatBox.Text = "F1";
+
+            doubleBox.Text = "1.23456";
+            this.LoseFocusButton.Click();
+
+            Assert.AreEqual(false, doubleBox.HasValidationError());
+            Assert.AreEqual("1.23456", doubleBox.Text);
+            Assert.AreEqual("1.2", doubleBox.FormattedView().Text);
+            Assert.AreEqual("1.23456", this.ViewModelValueBox.Text);
             Assert.AreEqual(TextSource.UserInput, doubleBox.TextSource());
 
+            this.StringFormatBox.Text = "F4";
             this.LoseFocusButton.Click();
             Assert.AreEqual(false, doubleBox.HasValidationError());
-            Assert.AreEqual(formatData.Text, doubleBox.Text);
-            Assert.AreEqual(formatData.Formatted, doubleBox.FormattedView().Text);
-            Assert.AreEqual(formatData.ViewModel, this.ViewModelValueBox.Text);
+            Assert.AreEqual("1.23456", doubleBox.Text);
+            Assert.AreEqual("1.2346", doubleBox.FormattedView().Text);
+            Assert.AreEqual("1.23456", this.ViewModelValueBox.Text);
+            Assert.AreEqual(TextSource.UserInput, doubleBox.TextSource());
+
+            this.StringFormatBox.Text = "F1";
+            this.LoseFocusButton.Click();
+            Assert.AreEqual(false, doubleBox.HasValidationError());
+            Assert.AreEqual("1.23456", doubleBox.Text);
+            Assert.AreEqual("1.2", doubleBox.FormattedView().Text);
+            Assert.AreEqual("1.23456", this.ViewModelValueBox.Text);
             Assert.AreEqual(TextSource.UserInput, doubleBox.TextSource());
         }
 
@@ -112,15 +97,15 @@ namespace Gu.Wpf.NumericInput.UITests.DoubleBox
             public readonly string StringFormat;
             public readonly CultureInfo Culture;
             public readonly string Formatted;
-            public readonly string ViewModel;
+            public readonly string ViewModelValue;
 
-            public FormatData(string text, string stringFormat, CultureInfo culture, string formatted, string viewModel)
+            public FormatData(string text, string stringFormat, CultureInfo culture, string formatted, string viewModelValue)
             {
                 this.Text = text;
                 this.StringFormat = stringFormat;
                 this.Culture = culture;
                 this.Formatted = formatted;
-                this.ViewModel = viewModel;
+                this.ViewModelValue = viewModelValue;
             }
 
             public override string ToString() => $"Text: {this.Text}, Expected: {this.Formatted}";
